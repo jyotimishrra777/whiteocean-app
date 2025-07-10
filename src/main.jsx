ca711dda9603752678c1fd59a58ab9ab372866c2
Anuj2508 <anuj.y@ecodedash.com>
2025-07-10 18:08:18 +0530

    Chnage the BrowserRouter To HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,17 @@ import "odometer/themes/odometer-theme-default.css";
 import "./styles/style1.css";
 import "./styles/style2.css";
 import "./styles/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ScrollToTop from "./user/customHooks/ScrollToTop.jsx";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter basename="/whiteocean-app/">
+      <HashRouter basename="/whiteocean-app/">
         <ScrollToTop />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </StrictMode>
 );
